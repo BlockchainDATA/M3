b40a72a9b6ef6dde7b5bf0aa9d102b237f83bd0f
dai <daihang1229@163.com>
2019-07-02 14:12:09 +0800

    增加样式 增加网络诊断工具

---
+++ demo-android/demo-netperf/src/main/java/eco/data/m3/demo/netperf/android/HtmlUtils.java
@@ -0,0 +1,43 @@
+package io.antube.app.utils;
+
+import android.text.Html;
+import android.text.Spanned;
+
+/**
+ * @description: textview 样式
+ * @author: dai
+ * @create date: 2018/6/7
+ * @update date: 2018/6/7
+ * @update description:
+ */
+public class HtmlUtils {
+    public static String getRedText(String count) {
+        return "观看带红包的内容奖励翻倍哦！当前时段还可领取" + count + "个红包内容，快快去视频列表内找红包吧～";
+    }
+
+    public static Spanned getRedTxtFormat(String startTxt, String redTxt, String endTxt) {
+        return Html.fromHtml(startTxt + "<font color='#FF5050'>" + redTxt + "</font>" +
+                "<font color='#666666'>" + endTxt + "</font>");
+    }
+
+    /**
+     * &emsp 空格
+     *
+     * @param front  前一个text
+     * @param behind 后一个text
+     * @return 最终text
+     */
+    public static Spanned setTvFormat(String front, String behind) {
+        return Html.fromHtml(front + "&emsp &emsp<font color='#333333'>" + behind + "</font>");
+    }
+
+    /**
+     * 尚未登录
+     *
+     * @return
+     */
+    public static Spanned getLogin() {
+        return Html.fromHtml("您未登录，<font color='#007aff'>登录</font><font color='#333333'>后可见</font>");
+    }
+
+}
--- demo-android/demo-netperf/src/main/java/eco/data/m3/demo/netperf/android/LogDialog.java
+++ demo-android/demo-netperf/src/main/java/eco/data/m3/demo/netperf/android/LogDialog.java
@@ -18,15 +18,75 @@ import android.widget.TextView;
 public class LogDialog extends DialogFragment {
     TextView contentTv;
     private String content;
+
+    public LogDialog() {
+        setStyle(STYLE_NO_FRAME, R.style.common_dialog_style);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_log, container, false);
         contentTv = view.findViewById(R.id.content_tv);
+        cancelTv = view.findViewById(R.id.cancel_tv);
+        confirmTv = view.findViewById(R.id.confirm_tv);
+        confirmTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(null!=mOnConfirmListener) {
+                    mOnConfirmListener.onConfirmClick();
+                }
+                dismiss();
+            }
+        });
+
+        cancelTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(null!=mOnCancelListener){
+                    mOnCancelListener.onCancelClick();
+                }
+                dismiss();
+            }
+        });
+
         contentTv.setMovementMethod(ScrollingMovementMethod.getInstance());
         contentTv.setText(content);
         return view;
     }
+
+
+    public void setOnConfirmListener(OnConfirmListener confirmListener) {
+        this.mOnConfirmListener = confirmListener;
+    }
+
+    public void setOnCancelListener(onCancelListener cancelListener) {
+        this.mOnCancelListener = cancelListener;
+    }
+
+    public interface OnConfirmListener {
+        void onConfirmClick();
+    }
+
+    public interface onCancelListener {
+        void onCancelClick();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            DisplayMetrics dm = new DisplayMetrics();
+            getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+            dialog.getWindow().setLayout((int) (dm.widthPixels * 0.8), ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+    }
+
+    private onCancelListener mOnCancelListener;
+    private OnConfirmListener mOnConfirmListener;
+
 
     public void setContent(String content) {
         this.content = content;
